794d052e3b148f65a98f1b2c16eaa5abfe0b27fb
Choi Han jun <gkswns3708@naver.com>
2021-07-06 14:28:51 +0900

    BOJ 11502번 세 개의 소수 문제

---
+++ 심화 스터디/1차시_수학,기하/BOJ 11502번 세 개의 소수 문제.py
@@ -0,0 +1,27 @@
+import sys
+
+T = int(sys.stdin.readline().strip())
+
+eratos_temp = [True] * (1000 + 1)
+eratos_temp[1] = False
+eratos = []
+for i in range(2, 1000 + 1):
+    if eratos_temp[i] == True:
+        eratos.append(i)
+    for j in range(i * 2, 1000 + 1, i):
+        eratos_temp[j] = False
+
+for TC in range(T):
+    K = int(sys.stdin.readline().strip())
+    flag = False
+    for i in eratos:
+        if flag:
+            break
+        for j in eratos:
+            if flag:
+                break
+            for k in eratos:
+                if i + j + k == K:
+                    print(i, j, k)
+                    flag = True
+                    break
