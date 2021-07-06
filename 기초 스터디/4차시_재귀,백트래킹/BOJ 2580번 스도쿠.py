3c2566b568e28f15e495b01825669e96f148244d
Choi Han jun <gkswns3708@naver.com>
2021-07-06 18:04:05 +0900

    4차시_재귀, 백트래킹

--- 기초 스터디/4차시_재귀,백트래킹/BOJ 2580번 스도쿠.py
+++ 기초 스터디/4차시_재귀,백트래킹/BOJ 2580번 스도쿠.py
@@ -1,35 +1,34 @@
-# 공부를 위한 Copy Code 입니다.
-a = [list(map(int, input().split())) for _ in range(9)]
-b, n = [0]*81, 0
-row = [[False]*10 for _ in range(9)]
-col = [[False]*10 for _ in range(9)]
-squ = [[False]*10 for _ in range(9)]
-
-def s(i, j):
-    return i//3*3 + j//3
-
-def solve(idx):
-    if idx == n:
-        for i in range(9):
-            print(' '.join(map(str, a[i])))
-        exit(0)
-    x, y = b[idx]//9, b[idx]%9
-    for i in range(1, 10):
-        if not (row[x][i] or col[y][i] or squ[s(x,y)][i]):
-            row[x][i] = col[y][i] = squ[s(x,y)][i] = True
-            a[x][y] = i
-            solve(idx+1)
-            a[x][y] = 0
-            row[x][i] = col[y][i] = squ[s(x,y)][i] = False
+arr = [list(map(int, input().split())) for _ in range(9)]
+empty_pos, N = [0] * 81, 0
+row = [[False] * 10 for _ in range(9)]
+col = [[False] * 10 for _ in range(9)]
+squ = [[False] * 10 for _ in range(9)]
 
 for i in range(9):
     for j in range(9):
-        k = a[i][j]
-        if k:
-            row[i][k] = True
-            col[j][k] = True
-            squ[s(i,j)][k] = True
+        if arr[i][j] != 0:
+            row[i][arr[i][j]] = True
+            col[j][arr[i][j]] = True
+            squ[(i // 3) * 3 + j // 3][arr[i][j]] = True
         else:
-            b[n] = i*9 + j
-            n += 1
+            empty_pos[N] = i * 9 + j
+            N += 1  # len 함수의 시간 복잡도가 O(N)이기 때문에 상수로 이렇게 구현해놓는게 편할 듯 하다.
+
+
+def solve(idx):
+    if idx == N:
+        for i in range(9):
+            print(' '.join(map(str, arr[i])))
+        exit(0)
+
+    x, y = empty_pos[idx] // 9, empty_pos[idx] % 9
+    for i in range(1, 10):
+        if not (row[x][i] or col[y][i] or squ[(x // 3) * 3 + y // 3][i]):
+            row[x][i] = col[y][i] = squ[(x // 3) * 3 + y // 3][i] = True
+            arr[x][y] = i
+            solve(idx + 1)
+            arr[x][y] = 0
+            row[x][i] = col[y][i] = squ[(x // 3) * 3 + y // 3][i] = False
+
+
 solve(0)
