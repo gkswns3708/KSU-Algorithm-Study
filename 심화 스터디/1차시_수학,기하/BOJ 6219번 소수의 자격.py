27f8f8ff5d0fc06b9c0bb93d65d095c59c7d3e6b
Choi Han jun <gkswns3708@naver.com>
2021-07-06 14:13:13 +0900

    BOJ 6219번 소수의 자격

--- 심화 스터디/1차시_수학,기하/BOJ 6219번 소수의 자격.py
+++ 심화 스터디/1차시_수학,기하/BOJ 6219번 소수의 자격.py
@@ -1,2 +1,18 @@
 import sys
 
+A, B, D = map(int, sys.stdin.readline().strip().split())
+
+eratos = [True] * 4000001
+eratos[1] = False
+cnt = 0
+
+for i in range(2, B + 1):
+    for j in range(i * 2, B + 1, i):
+        eratos[j] = False
+
+for i in range(A, B + 1):
+    if eratos[i] == True:
+        if str(D) in str(i):
+            cnt += 1
+
+print(cnt)
